85be1227bfba5366cc140285fffa9e1513d2c91a
nxtcoder36 <mohit0698@gmail.com>
2024-07-01 17:33:13 +0530

    healthcheck time interval reduced

--- wireguard/healthcheck.sh
+++ wireguard/healthcheck.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# Check if the target IP is reachable
 if ping -c 1 100.64.0.1 > /dev/null 2>&1
 then
-    exit 0  # IP is reachable, healthcheck passes
+    exit 0
 else
-    exit 1  # IP is not reachable, healthcheck fails
+    exit 1
 fi
