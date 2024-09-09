097ee8df60a960cc45fd896146cba74766789727
nxtcoder36 <mohit0698@gmail.com>
2024-09-09 13:09:58 +0530

    wip

--- domain/apiclient/k3s-local.go
+++ domain/apiclient/k3s-local.go
@@ -4,4 +4,9 @@ import "github.com/kloudlite/kl/domain/fileclient"
 
 func (apic *apiClient) GetClusterConfig(account string) (*fileclient.AccountClusterConfig, error) {
 	clusterConfig, err := apic.fc.GetClusterConfig(account)
+	if err != nil {
+		return nil, err
+	}
+	// TODO: check if cluster config is valid and if not, create it
+	return clusterConfig, nil
 }
