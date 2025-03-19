502406f17a8b3d6fd9e2d1e97484f802f88d1fa7
Abdhesh Nayak <abdheshnayak@gmail.com>
2025-03-19 23:10:39 +0530

    :bug: Fix

--- cmd/packages/shell/checkcommand.go
+++ cmd/packages/shell/checkcommand.go
@@ -16,37 +16,36 @@ var CheckCmd = &cobra.Command{
 	Use:    "checkchanges",
 	Hidden: true,
 	Run: func(cmd *cobra.Command, args []string) {
-		s, _ := clients.File.DirEnv()
+		denv, _ := clients.File.DirEnv()
 
-		if s == "" {
-			s = "no env"
-		} else {
+		if denv == "" {
+			denv = "no env"
 		}
 
 		ck, err := getCache()
 		if err != nil || ck.Hash != os.Getenv("KL_HASH") {
 			if err == fileclient.KLYamlNotFound {
-				fmt.Printf("(%s - %s)", s, text.Yellow("outside"))
+				fmt.Printf("(%s - %s)", denv, text.Yellow("outside"))
 				return
 			}
-			fmt.Printf("(%s - %s)", s, text.Yellow("reload needed"))
+			fmt.Printf("(%s - %s)", denv, text.Yellow("reload needed"))
 			return
 		}
 
 		depth := 0
-		s, ok := os.LookupEnv("KL_DEPTH")
+		ldepth, ok := os.LookupEnv("KL_DEPTH")
 		if ok {
-			i, err := strconv.Atoi(s)
+			i, err := strconv.Atoi(ldepth)
 			if err == nil {
 				depth = i
 			}
 		}
 		if depth > 1 {
-			fmt.Printf("%s%s%s", text.Blue(fmt.Sprintf("(%s", s)), text.Yellow(strings.Repeat(">", depth-1)), text.Blue(")"))
+			fmt.Printf("%s%s%s", text.Blue(fmt.Sprintf("(%s", denv)), text.Yellow(strings.Repeat(">", depth-1)), text.Blue(")"))
 			return
 		}
 
-		fmt.Printf(text.Blue(fmt.Sprintf("(%s)", s)))
+		fmt.Printf(text.Blue(fmt.Sprintf("(%s)", denv)))
 		return
 	},
 }
